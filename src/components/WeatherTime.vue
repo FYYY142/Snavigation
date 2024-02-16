63906fb5915baa5543887cbbd7d4294a2ee7ecaa
fyyy142 <2720880073@qq.com>
2024-02-16 10:42:50 +0800

    删除天气

--- src/components/WeatherTime.vue
+++ src/components/WeatherTime.vue
@@ -43,10 +43,12 @@
       <span class="weekday">{{ timeData.weekday ?? "星期八" }}</span>
     </div>
     <div v-if="weatherShow && set.showWeather" class="weather">
+      <!--
       <span class="status">{{ weatherData.condition ?? "N/A" }}</span>
       <span class="temperature">{{ weatherData.temp ?? "N/A" }} ℃</span>
       <span class="wind">{{ weatherData.windDir ?? "N/A" }}</span>
       <span v-if="weatherData.windLevel" class="wind-level"> {{ weatherData.windLevel }} 级 </span>
+      -->
     </div>
   </div>
 </template>
